84fc628232ab4618ff7dcd78d6a24030c9d3e119
François Blackburn <fblackburn@avencall.com>
2016-01-22 16:34:32 -0500

    setup.py: add xivo_lib_rest_client into requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2015 Avencall
+# Copyright (C) 2015-2016 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,6 +18,14 @@
 
 from setuptools import setup
 from setuptools import find_packages
+
+requirements = [
+    "xivo_lib_rest_client==0.2",
+]
+
+dependency_links = [
+    'git+https://github.com/xivo-pbx/xivo-lib-rest-client.git#egg=xivo_lib_rest_client-0.2'
+]
 
 setup(
     name='xivo_auth_client',
@@ -37,5 +45,7 @@ setup(
             'token = xivo_auth_client.commands.token:TokenCommand',
             'backends = xivo_auth_client.commands.backends:BackendsCommand',
         ],
-    }
+    },
+    install_requires=requirements,
+    dependency_links=dependency_links
 )
