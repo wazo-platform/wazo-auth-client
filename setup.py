5b97c863097a0b7af0a9cd7468f5972e1025854e
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2016-11-28 11:20:28 -0500

    setup.py: xivo-pbx -> wazo-pbx

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     author='Avencall',
     author_email='dev@avencall.com',
 
-    url='https://github.com/xivo-pbx/xivo-auth-client',
+    url='https://github.com/wazo-pbx/xivo-auth-client',
 
     packages=find_packages(),
 
