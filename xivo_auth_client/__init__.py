2037b329cb20faa2560e5ca9577ab4fdc5bdabdb
Francois Blackburn <fblackburn@wazo.community>
2019-02-03 11:55:56 -0500

    update SPDX-License-Identifier

--- xivo_auth_client/__init__.py
+++ xivo_auth_client/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2015 Avencall
-# SPDX-License-Identifier: GPL-3.0+
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_auth_client.client import AuthClient as Client
