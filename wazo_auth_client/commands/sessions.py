f2a2ec3ee373b058a1d7aacba189bad55670c0d8
Charles Langlois <charles.langlois@wazo.io>
2024-01-05 16:22:01 -0500

    commands.sessions: annotations

--- wazo_auth_client/commands/sessions.py
+++ wazo_auth_client/commands/sessions.py
@@ -2,20 +2,24 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 from __future__ import annotations
 
+from typing import Any
+
 from wazo_lib_rest_client import RESTCommand
+
+from .._types import JSON
 
 
 class SessionsCommand(RESTCommand):
     resource = 'sessions'
 
-    def list(self, **kwargs):
+    def list(self, **kwargs: Any) -> JSON:
         headers = self._get_headers(**kwargs)
         r = self.session.get(self.base_url, headers=headers, params=kwargs)
         if r.status_code != 200:
             self.raise_from_response(r)
         return r.json()
 
-    def delete(self, session_uuid, tenant_uuid=None):
+    def delete(self, session_uuid: str, tenant_uuid: str | None = None) -> None:
         headers = self._get_headers(tenant_uuid=tenant_uuid)
         url = f'{self.base_url}/{session_uuid}'
         r = self.session.delete(url, headers=headers)
