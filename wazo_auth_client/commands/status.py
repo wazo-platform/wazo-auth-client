24fdcd23473ef74fcd6017108b7daab552f1b10b
Charles Langlois <charles.langlois@wazo.io>
2024-01-05 16:20:31 -0500

    commands.status: annotation

--- wazo_auth_client/commands/status.py
+++ wazo_auth_client/commands/status.py
@@ -8,7 +8,7 @@ from wazo_lib_rest_client import RESTCommand
 class StatusCommand(RESTCommand):
     resource = 'status'
 
-    def check(self):
+    def check(self) -> None:
         headers = self._get_headers()
         r = self.session.head(self.base_url, headers=headers)
         if r.status_code != 200:
