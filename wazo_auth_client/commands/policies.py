69e40d6f4ddf2c3e6d2d6f4ccdcf3d9da8594743
Charles Langlois <charles.langlois@wazo.io>
2024-01-05 16:27:02 -0500

    commands.policies: annotations

--- wazo_auth_client/commands/policies.py
+++ wazo_auth_client/commands/policies.py
@@ -2,15 +2,18 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 from __future__ import annotations
 
+from typing import Any
 from urllib.parse import quote
 
 from wazo_lib_rest_client import RESTCommand
+
+from .._types import JSON
 
 
 class PoliciesCommand(RESTCommand):
     resource = 'policies'
 
-    def add_access(self, policy_uuid, access):
+    def add_access(self, policy_uuid: str, access: str) -> None:
         headers = self._get_headers()
         access = quote(access)
         url = f'{self.base_url}/{policy_uuid}/acl/{access}'
@@ -18,14 +21,14 @@ class PoliciesCommand(RESTCommand):
         if r.status_code != 204:
             self.raise_from_response(r)
 
-    def delete(self, policy_uuid):
+    def delete(self, policy_uuid: str) -> None:
         headers = self._get_headers()
         url = f'{self.base_url}/{policy_uuid}'
         r = self.session.delete(url, headers=headers)
         if r.status_code != 204:
             self.raise_from_response(r)
 
-    def edit(self, policy_uuid, name, **kwargs):
+    def edit(self, policy_uuid: str, name: str, **kwargs: Any) -> JSON:
         headers = self._get_headers()
         url = f'{self.base_url}/{policy_uuid}'
         kwargs['name'] = name
@@ -34,7 +37,7 @@ class PoliciesCommand(RESTCommand):
             self.raise_from_response(r)
         return r.json()
 
-    def get(self, policy_uuid):
+    def get(self, policy_uuid: str) -> JSON:
         headers = self._get_headers()
         url = f'{self.base_url}/{policy_uuid}'
         r = self.session.get(url, headers=headers)
@@ -42,14 +45,14 @@ class PoliciesCommand(RESTCommand):
             self.raise_from_response(r)
         return r.json()
 
-    def list(self, tenant_uuid=None, **kwargs):
+    def list(self, tenant_uuid: str | None = None, **kwargs: Any) -> JSON:
         headers = self._get_headers(tenant_uuid=tenant_uuid)
         r = self.session.get(self.base_url, headers=headers, params=kwargs)
         if r.status_code != 200:
             self.raise_from_response(r)
         return r.json()
 
-    def new(self, name, tenant_uuid=None, **kwargs):
+    def new(self, name: str, tenant_uuid: str | None = None, **kwargs: Any) -> JSON:
         headers = self._get_headers(tenant_uuid=tenant_uuid)
         kwargs['name'] = name
         r = self.session.post(self.base_url, headers=headers, json=kwargs)
@@ -57,7 +60,7 @@ class PoliciesCommand(RESTCommand):
             self.raise_from_response(r)
         return r.json()
 
-    def remove_access(self, policy_uuid, access):
+    def remove_access(self, policy_uuid: str, access: str) -> None:
         headers = self._get_headers()
         access = quote(access)
         url = f'{self.base_url}/{policy_uuid}/acl/{access}'
