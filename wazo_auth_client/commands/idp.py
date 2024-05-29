56c18e69745e9f99b4e24d11ca5d7f194c8ea2fd
Pascal Cadotte Michaud <pcm@wazo.io>
2024-05-29 14:00:59 -0400

    idp: add commands to manage idp user association

--- wazo_auth_client/commands/idp.py
+++ wazo_auth_client/commands/idp.py
@@ -18,3 +18,35 @@ class Command(RESTCommand):
         if r.status_code != 200:
             self.raise_from_response(r)
         return r.json()
+
+    def add_user(self, idp_type: str, user_uuid: str) -> None:
+        headers = self._get_headers()
+        url = self._relation_url('users', idp_type, user_uuid)
+        r = self.session.put(url, headers=headers)
+        if r.status_code != 204:
+            self.raise_from_response(r)
+
+    def add_users(self, idp_type: str, users: JSON) -> None:
+        headers = self._get_headers()
+        url = self._relation_url('users', idp_type)
+        r = self.session.put(url, headers=headers, json={'users': users})
+        if r.status_code != 204:
+            self.raise_from_response(r)
+
+    def remove_user(self, idp_type: str, user_uuid: str) -> None:
+        headers = self._get_headers()
+        url = self._relation_url('users', idp_type, user_uuid)
+        r = self.session.delete(url, headers=headers)
+        if r.status_code != 204:
+            self.raise_from_response(r)
+
+    def _relation_url(
+        self,
+        resource: str,
+        idp_type: str,
+        resource_uuid: str | None = None,
+    ) -> str:
+        if resource_uuid:
+            return '/'.join([self.base_url, idp_type, resource, resource_uuid])
+        else:
+            return '/'.join([self.base_url, idp_type, resource])
